374be48a527e7c34e501a7ece25c2972f9a4c59c
Guillem Muñoz <gilli11519@gmail.com>
2025-01-11 13:50:34 +0100

    probuilder and phisics

--- Assets/Scripts/Trineo.cs
+++ Assets/Scripts/Trineo.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
+
 public class Trineo : MonoBehaviour
 {
-    public float velocidad = 3.0f;
+    private Rigidbody rb;
+    [SerializeField] private float velocidad;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-    
+        rb = this.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -15,6 +18,7 @@ public class Trineo : MonoBehaviour
         float hInput = Input.GetAxisRaw("Horizontal");
         float VInput = Input.GetAxisRaw("Vertical");
         Vector3 direccionMovimiento = new Vector3 (hInput, 0, VInput).normalized;
-        transform.position += direccionMovimiento * velocidad * Time.deltaTime;
+        //transform.position += direccionMovimiento * velocidad * Time.deltaTime;
+        transform.Translate(direccionMovimiento * velocidad * Time.deltaTime);
     }
 }

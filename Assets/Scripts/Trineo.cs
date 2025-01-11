18020635b12c57eb6f1ab8d135cd3ffc655483d9
Guillem Muñoz <gilli11519@gmail.com>
2025-01-11 12:21:54 +0100

    old input sistem

--- Assets/Scripts/Trineo.cs
+++ Assets/Scripts/Trineo.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class Trineo : MonoBehaviour
 {
+    public float velocidad = 3.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+    
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float hInput = Input.GetAxisRaw("Horizontal");
+        float VInput = Input.GetAxisRaw("Vertical");
+        Vector3 direccionMovimiento = new Vector3 (hInput, 0, VInput).normalized;
+        transform.position += direccionMovimiento * velocidad * Time.deltaTime;
     }
 }
